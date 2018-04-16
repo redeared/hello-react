6191f34cb07cb843406a89f52d7486ebbaf03380
Andrey Misaylov <misailovaa@gmail.com>
2018-04-16 17:08:18 +0300

    follow recommendation

--- public/js/fourth.js
+++ public/js/fourth.js
@@ -1,6 +1,6 @@
 
-const FourthGreeting = ({ visitor: visitor = 'World!', ...props }) => {
-    return <h1> Hello{visitor ? " " + visitor: ''}. From fourth - functional component </h1>;
+const FourthGreeting = ({ visitor = 'World!' }) => {
+    return <h1> Hello{visitor && ' '}{visitor}. From fourth - functional component </h1>;
 }
 
 ReactDOM.render(<FourthGreeting visitor="World" />, document.getElementById('fourth'));
